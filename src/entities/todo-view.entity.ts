81b8dcbfa382fd1a411a016500b3dc69d051dae3
dunn <dunglethe04@gmail.com>
2024-09-08 08:56:28 +0700

    fix: Just understand what View is in database.

--- src/entities/todo-view.entity.ts
+++ src/entities/todo-view.entity.ts
@@ -3,17 +3,17 @@ import { UserEntity } from "./user.entity";
 import { TodoEntity } from "./todo.entity";
 
 @ViewEntity({
-  name: "todos",
-  expression: (connection) =>
-    connection
-      .createQueryBuilder()
-      .select("todo.id", "id")
-      .addSelect("todo.title", "title")
-      .addSelect("todo.description", "description")
-      .addSelect("todo.status", "status")
-      .addSelect("users.username", "author")
-      .from(TodoEntity, "todo")
-      .leftJoin(UserEntity, "users", "users.id = todo.user_id"),
+  name: "todos_author_view",
+  // expression: (connection) =>
+  //   connection
+  //     .createQueryBuilder()
+  //     .select("todo.id", "id")
+  //     .addSelect("todo.title", "title")
+  //     .addSelect("todo.description", "description")
+  //     .addSelect("todo.status", "status")
+  //     .addSelect("users.username", "author")
+  //     .from(TodoEntity, "todo")
+  //     .leftJoin(UserEntity, "users", "users.id = todo.user_id"),
 })
 export class TodoViewEntity {
   @ViewColumn()
